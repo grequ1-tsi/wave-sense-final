cc35fee90154593874ffd38c6f68a3e5d265fb8a
grequ1-tsi <miguelgrequi.pl022@academico.ifsul.edu.br>
2025-01-12 02:47:49 -0300

    Teste JSON

--- app/Http/Controllers/Api/MovimentoController.php
+++ app/Http/Controllers/Api/MovimentoController.php
@@ -42,7 +42,7 @@ class MovimentoController extends Controller
         $name = $json['name'];
         $item = $json['item'];
         $datetime = $json['datetime'];
-        return Log::info($item, $datetime);
+        return Log::info(print_r($item, true));
         //return response()->json($Movimento);
     }
 
